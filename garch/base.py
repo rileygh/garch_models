0a4d3e361260420b9db56f721ad67a50abe4a9be
rileygh <riley.s.hewitt@proton.me>
2026-02-05 03:47:51 +0000

    draft base garch

--- garch/base.py
+++ garch/base.py
@@ -4,6 +4,7 @@ Abstract base class (ABC) for GARCH-family models
 
 from abc import ABC, abstractmethod
 import numpy as np
+from numpy.typing import NDArray
 from scipy.optimize import minimize
 
 class BaseGARCH(ABC):
@@ -38,17 +39,17 @@ class BaseGARCH(ABC):
         -> time-series of returns
         '''
 
-        self.returns = np.array(returns)
-        self.T = len(returns)
-        self.params = None
-        self.sigma2 = None
-        self.epsilon = None
-        self.std_residuals = None
-        self.log_likelihood = None
-        self._fitted = False
+        self.returns: NDArray[np.float64] = np.asarray(returns, dtype=np.float64)
+        self.T: int = len(returns)
+        self.params: NDArray[np.float64] | None = None
+        self.sigma2: NDArray[np.float64] | None = None
+        self.epsilon: NDArray[np.float64] | None = None
+        self.std_residuals: NDArray[np.float64] | None = None
+        self.log_likelihood: float | None = None
+        self._fitted: bool = False
 
     @abstractmethod
-    def get_initial_params(self):
+    def get_initial_params(self) -> NDArray[np.float64]:
         '''
         Gets initial parameter values for optimisation
 
@@ -59,7 +60,18 @@ class BaseGARCH(ABC):
         pass
 
     @abstractmethod
-    def get_constraints(self):
+    def get_param_names(self) -> list[str]:
+        '''
+        Different models have different parameter names, so this retrieves them for display
+
+        Returns:
+        param_names : list[str]
+        -> parameter names (ordered)
+        '''
+        pass
+
+    @abstractmethod
+    def get_constraints(self) -> tuple[dict]:
         '''
         Gets constraints for parameters used in optimisation
 
@@ -69,7 +81,7 @@ class BaseGARCH(ABC):
         '''
 
     @abstractmethod
-    def _compute_variance_recursion(self, params, epsilon, sigma2):
+    def _compute_variance_recursion(self, params, epsilon, sigma2) -> NDArray[np.float64]:
         '''
         Computes conditional variance using model-specific recursion
 
@@ -88,7 +100,7 @@ class BaseGARCH(ABC):
         pass
 
     @abstractmethod
-    def forecast(self, horizon=1):
+    def forecast(self, horizon=1) -> NDArray[np.float64]:
         '''
         Forecasts variance for future periods (horizon as the no. of periods)
 
@@ -102,7 +114,7 @@ class BaseGARCH(ABC):
         '''
         pass
 
-    def _log_likelihood(self, params):
+    def _log_likelihood(self, params) -> float:
         '''
         Computes the log-likelihood
 
@@ -115,11 +127,11 @@ class BaseGARCH(ABC):
         -> log-likelihood value
         '''
 
-        mu = params[0]
-        model_params = params[1:]
-        sigma2 = np.zeros(self.T)
+        mu: float = params[0]
+        model_params: NDArray[np.float64] = params[1:]
+        sigma2: NDArray[np.float64] = np.zeros(self.T)
         sigma2[0] = np.var(self.returns) # initial variance taken from return series
-        epsilon = self.returns - mu # residuals
+        epsilon: NDArray[np.float64] = self.returns - mu # residuals
 
         # populate sigma2 with model-specific formula for conditional variance
         sigma2 = self._compute_variance_recursion(model_params, epsilon, sigma2)
@@ -127,8 +139,79 @@ class BaseGARCH(ABC):
         if not np.all(sigma2 > 0): raise ValueError('require all variances to be positive')
         
         # applying log-likelihood formula
-        log_likelihood = -0.5 * np.sum(np.log(2 * np.pi) + np.log(sigma2) - (epsilon ** 2 / sigma2))
+        log_likelihood: float = -0.5 * np.sum(np.log(2 * np.pi) + np.log(sigma2) - (epsilon ** 2 / sigma2))
 
         return log_likelihood
     
+    def _compute_conditional_variance(self) -> NDArray[np.float64]:
+        '''
+        Computes conditional variances using fitted parameters
+
+        Returns:
+        conditional_var : np.ndarray
+        -> conditional variance array
+        '''
+
+        mu: float = self.params[0]
+        model_params = self.params[1:]
+        
+        conditional_var: NDArray[np.float64] = np.zeros(self.T)
+        conditional_var[0] = np.var(self.returns)
+
+        self.epsilon = self.returns - mu
+
+        conditional_var = self._compute_variance_recursion(model_params, self.epsilon, conditional_var)
+
+        self.std_residuals = self.epsilon / np.sqrt(conditional_var)
+
+        return conditional_var
     
+    def fit(self, initial_params=get_initial_params(), method='SLSQP', options={'maxiter': 1000}):
+        '''
+        Estimates model parameters using maximum likelihood estimation (MLE)
+
+        Parameters:
+        initial_params : np.ndarray | None
+        -> initial parameter values (defaults available if none given)
+        method : str - default SLSQP
+        -> optimisation method for scipy.optimize.minimize
+        options : dict | None
+        -> additional options
+        '''
+
+        constraints: tuple[dict] = self.get_constraints()
+
+        res = minimize(-self.log_likelihood, initial_params, method=method, constraints=constraints, options=options)
+
+        if not res.success: raise RuntimeError(f'optimisation did not converge. message: {res.message}')
+
+        self.params = res.x # solution array
+        self.log_likelihood = res.fun
+        self.sigma2 = self._compute_conditional_variance()
+        self._fitted = True
+
+        return self
+
+    def summary(self):
+        '''
+        Provides a summary of parameter estimates and model statistics
+        '''
+        if not self._fitted:
+            raise RuntimeError('model not fitted. make sure that you have called the fit method before trying to display a summary')
+        
+        param_names = self.get_param_names()
+
+        print(f'\n{self.__class__.__name__} Model Estimates\n{'='*60}')
+
+        for n, v in zip(param_names, self.params):
+            print(f'{n:25s}: {v:.6e}' if abs(v) < 0.01 else f'{n:25s}: {v:.6f}')
+        
+        print(f'{'Log-likelihood':25s}: {self.log_likelihood:.2f}')
+
+        # info criteria AIC/BIC
+        k = len(self.params)
+        aic = -2 * self.log_likelihood + 2*k
+        bic = -2 * self.log_likelihood + k*np.log(self.T)
+
+        print(f'Information criteria:\n{'AIC':25s}: {aic:.2f}\n{'BIC':25s}: {bic:.2f}\n{'='*60}')
+        
